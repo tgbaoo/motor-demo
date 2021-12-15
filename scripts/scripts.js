d8a957b9436c9b6ae3a947cb6ff507a688a30692
root <root@thepi.localdomain>
2021-12-16 00:21:01 +0700

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById('modal-register-wrapper');
+window.onclick=function(event){
+    if(event.target==modal){
+        modal.style.display="none"
+    }
+}
+
+var modal = document.getElementById('modal-login-wrapper');
+window.onclick=function(event){
+    if(event.target==modal){
+        modal.style.display="none"
+    }
+}
+
+var modal = document.getElementById('switch-to-login');
+window.onclick=function(event){
+    if(event.target==modal){
+        modal.style.display="none"
+    }
+}
